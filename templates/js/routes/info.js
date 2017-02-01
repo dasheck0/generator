e9d93409fd8b08a3b725d50db03e17bb2a014bd2
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-02-01 15:10:14 +0100

    Fix intendation bugs

--- templates/js/routes/info.js
+++ templates/js/routes/info.js
@@ -4,10 +4,10 @@ const info = require('../../../package.json');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({
-        name: info.name,
-        version: info.version
-    });
+  res.json({
+    name: info.name,
+    version: info.version
+  });
 });
 
 module.exports = router;
